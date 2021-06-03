6e39ccced40e13c102cc29fdf7dcdaaf63180a06
abhishekgupta23 <53884462+abhishekgupta23@users.noreply.github.com>
2021-06-03 17:36:21 +0530

    Update image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -7,7 +7,7 @@ st.markdown(
     """
     <style>
     .reportview-container {
-        background: url("https://images.unsplash.com/photo-1552168324-d612d77725e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80")no-repeat center fixed;
+        background: url("https://images.unsplash.com/photo-1496715976403-7e36dc43f17b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80")no-repeat center fixed;
         background-size: cover;
     }
     </style>
