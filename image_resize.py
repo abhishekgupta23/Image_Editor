83369284ea62843cf34501bce3a2c1bfa75bf3d9
abhishekgupta23 <53884462+abhishekgupta23@users.noreply.github.com>
2021-06-03 17:27:21 +0530

    Update image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -8,7 +8,6 @@ st.markdown(
     <style>
     .reportview-container {
         background: url("https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80")
-        background-size: cover;
     }
    .sidebar .sidebar-content {
         background: url("url_goes_here")
