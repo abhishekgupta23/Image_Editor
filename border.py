fbc8e2aa0798fdb2fb30f17b3d091f33ebfef4ca
abhishekgupta23 <53884462+abhishekgupta23@users.noreply.github.com>
2021-06-04 12:40:47 +0530

    Add files via upload

---
+++ border.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from PIL import Image, ImageOps
+import base64 
+from io import BytesIO
+def app():
+    st.markdown(
+        """
+        <style>
+        .reportview-container {
+            background: url("https://images.unsplash.com/photo-1558244402-286dd748c593?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80") no-repeat center fixed;
+            background-size: cover;
+        }
+    .sidebar .sidebar-content {
+            background: url("url_goes_here")
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.title("Add Border To Image")
+    im1=st.file_uploader(label="UPLOAD IMAGE")
+    # open the image
+    img = Image.open("im1")
+    # add border to the image
+    img2 = ImageOps.expand(img, border=20, fill='blue')
+    # display image
+    img2.show()
+    btn = st.button("Save")
+    if btn:
+        buffered = BytesIO()
+        img2.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        href = f'<a href="data:file/jpg;base64,{img_str}" download="final image.jpg"><h1>Download final image</h1></a>'
+        st.markdown(href, unsafe_allow_html=True)
